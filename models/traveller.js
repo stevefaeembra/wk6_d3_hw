9219f823fbc6e026667082683164c1a7dc0b8aa3
Steven Kay <stevendkay@gmail.com>
2018-11-21 16:30:28 +0000

    getting journey start locations

--- models/traveller.js
+++ models/traveller.js
@@ -3,7 +3,10 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  // get start location for each
+  return this.journeys.map((item) => {
+    return item.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
