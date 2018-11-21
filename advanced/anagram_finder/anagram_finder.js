5ae341f6afd68469ef058fad437ab49312807593
Steven Kay <stevendkay@gmail.com>
2018-11-21 19:37:06 +0000

    extensions anagram.. got first two tests passing, added tests for utility methods

--- advanced/anagram_finder/anagram_finder.js
+++ advanced/anagram_finder/anagram_finder.js
@@ -1,9 +1,32 @@
 const AnagramFinder = function (word) {
-
+  this.word = word;
 }
+
+AnagramFinder.prototype.arraysIdentical = function (arrayOne, arrayTwo) {
+  // check arrays are identical
+  // first create copies as this is destructive to arrayTwo
+  const arrayOneCopy = arrayOne.slice(0);
+  const arrayTwoCopy = arrayTwo.slice(0);
+  // if different length, can't be identical
+  if (arrayOne.length != arrayTwo.length) return false;
+  // same length, should pop from start (shift) each one and
+  // compare to next in array.
+  return arrayOne.every((item) => {
+    return item === arrayTwoCopy.shift();
+  })
+};
+
+AnagramFinder.prototype.sortLetters = function (word) {
+  // two words are anagrams if the sorted set of letters
+  // are identical. e.g god -> [d,g,o], dog -> [d,g,o]
+  // good -> [d,g,o,o] so not an anogram
+  return word.split("").sort();
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  return otherWords.filter((word) => {
+    return this.arraysIdentical(this.sortLetters(word), this.sortLetters(this.word));
+  })
 }
 
-module.exports = AnagramFinder;
+module.exports = AnagramFinder
--- advanced/anagram_finder/anagram_finder_spec.js
+++ advanced/anagram_finder/anagram_finder_spec.js
@@ -2,12 +2,29 @@ const assert = require('assert');
 const AnagramFinder = require('./anagram_finder.js');
 
 describe('AnagramFinder', function () {
+
+  // i added a couple in here for my utility methods
+
+  it('should be able to sort a string into an array of letters', function () {
+    const anagramFinder = new AnagramFinder('castle');
+    const actual = anagramFinder.sortLetters("castle");
+    const expected = ['a','c','e','l','s','t'];
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it('should be possible to confirm two arrays are identical', function () {
+    const anagramFinder = new AnagramFinder('castle');
+    assert.strictEqual(anagramFinder.arraysIdentical([1,2,3],[1,2,3]), true);
+    assert.strictEqual(anagramFinder.arraysIdentical([1,2,3],[]), false);
+    assert.strictEqual(anagramFinder.arraysIdentical([1,2,3],[1,2,3,4]), false);
+  });
+
   it('should be able to detect an anagram', function () {
     const anagramFinder = new AnagramFinder('act');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['cat', 'dog']), ['cat']);
   });
 
-  xit('should be able to detect a non-anagram', function () {
+  it('should be able to detect a non-anagram', function () {
     const anagramFinder = new AnagramFinder('potato');
     assert.deepStrictEqual(anagramFinder.findAnagrams(['tomato']), []);
   })
