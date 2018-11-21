74dc7801d54ffbba48dee1eabbf6cbdaff9f25f1
Steven Kay <stevendkay@gmail.com>
2018-11-21 20:40:09 +0000

    two additional tests re-enabled, passing with no further work

--- advanced/pangram_finder/pangram_finder_spec.js
+++ advanced/pangram_finder/pangram_finder_spec.js
@@ -30,12 +30,12 @@ describe('PangramFinder', function () {
     assert.strictEqual(pangramFinder.isPangram(), false);
   });
 
-  xit('should be able to detect a pangram with special characters', function () {
+  it('should be able to detect a pangram with special characters', function () {
     const pangramFinder = new PangramFinder('how_vexingly_quick_daft_zebras_jump!');
     assert.strictEqual(pangramFinder.isPangram(), true);
   });
 
-  xit('should be able to detect a non-pangram with special characters', function () {
+  it('should be able to detect a non-pangram with special characters', function () {
     const pangramFinder = new PangramFinder('is_this_a_pangram?!');
     assert.strictEqual(pangramFinder.isPangram(), false);
   });
